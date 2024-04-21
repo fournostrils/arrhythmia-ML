f1bca30d78f0970f350b0d72ffcd42008774b11f
Harrison Pham <109564464+fournostrils@users.noreply.github.com>
2024-04-21 01:10:53 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,11 +1,8 @@
 import wfdb
-from wfdb import processing
 import pandas as pd
 import numpy as np
 from scipy.signal import medfilt, find_peaks, butter, lfilter
 import matplotlib.pyplot as plt
-import pywt # type: ignore
-
 
 # Function to save ECG data to CSV
 def save_ecg_to_csv(record_name):
@@ -25,7 +22,6 @@ def save_ecg_to_csv(record_name):
     df = pd.DataFrame(columns=['Time']+lead_names)
     df[lead_names] = ecg_data
     df['Time'] = time
-
 
 ################# median filter ###################
     baseline = medfilt(df[lead_names[0]], 71)       #cite the guy for this
@@ -64,7 +60,6 @@ def save_ecg_to_csv(record_name):
     df.to_csv(filename, index=False)
     print(f'Saved {filename}')
     
-
 def feature_extract(record_name):
     
     def threshold_peaks(data, threshold_value=0.5):
@@ -73,8 +68,7 @@ def feature_extract(record_name):
         peaks, _ = find_peaks(data, height=threshold)
         return peaks
 
-
-    data = pd.read_csv(f"C:\\Users\\rigga\\Documents\\BMEN 207\\Honors project\\{record_name}.csv")
+    data = pd.read_csv(f"C:\\Users\\pharr\\OneDrive\\Desktop\\PYTHON\\{record_name}.csv")
     lead = data.columns[1]
 
     data['gradient'] = np.gradient(data[lead])        #first derivative peak is very reliably in the middle of the increasing part of the r wave
@@ -83,14 +77,6 @@ def feature_extract(record_name):
     peak_dev1 = r_peaks
     print(peak_dev1)
     
-    for i in range(2):
-        data['gradient'] = np.gradient(data['gradient'])  #third derivative usually finds middle of descending part of r wave
-        r_peaks = find_peaks(np.asfarray(data['gradient']),prominence = 0.3*max(data['gradient']), distance = 72, height=0.25*max(data['gradient']))[0]
-        print(f'{i+2} derivative r_peaks length= {len(r_peaks)}')
-    
-    #r_peaks = (peak_dev1 + r_peaks) /2
-    
-
     # Read annotation data
     ann = wfdb.rdann(record_name, 'atr', pn_dir='mitdb')
 
@@ -98,49 +84,52 @@ def feature_extract(record_name):
     symbols = ann.symbol
     sample_indices = ann.sample
     #time_points = sample_indices/sampling_frequency
-####################### harrison code #################
-    check = np.where(np.logical_or(symbols=='~',symbols=='|'))[0]
 
-    sample_indices=np.delete(sample_indices,check, axis=0)
-    symbols=np.delete(symbols,check, axis=0)
+##################### harrison code #################
 
-    #find indices of r wave peaks
-    #r_peaks = find_peaks(np.asfarray(data['gradient']),prominence = 0.3*max(data['gradient']), distance = 72, height=0.25*max(data['gradient']))[0]
-        #find r-peaks missed
-    #r2r_peaks = np.diff(r_peaks)
-        #check=np.where(r2r_peaks>400 )[0]
-    #r_peaks = np.insert(r_peaks, check+1,(r_peaks[check]+r_peaks[check+1])/2)
-    '''
-    if len(r_peaks) > len(sample_indices):
-        r_peaks = r_peaks[:len(sample_indices)]
-    elif len(r_peaks) < len(sample_indices):
-        sample_indices = sample_indices[:len(r_peaks)]'''
-#######################################################
+    #validate sample_indices - delete symbols that show artifacts
+    #check= np.where(np.logical_or(symbols=='~',symbols=='|'))[0] 
 
-    #find indices of r wave peaks
-    #r_peaks = find_peaks(np.asfarray(data['Both filters']),prominence = 0.3*max(data['Both filters']), distance = 72)[0]
+    #sample_indices=np.delete(sample_indices,check, axis=0)
+    #symbols=np.delete(symbols,check, axis=0)
 
-    '''
-    for i in range(len(r_peaks)-1):
-        if abs(fwd_derivative[i]) < abs(mean_fwd_dev):
-            r_peaks.pop(i)
-    '''
-    output_x = np.array([])
-    output_y = np.array([])
+    #validate sample_indices - check for really close and really far annotations
+    #check = np.where(np.logical_or(np.diff(sample_indices)>1.75*np.mean(np.diff(sample_indices)),np.diff(sample_indices)<.*np.mean(np.diff(sample_indices))))
+    #check = np.array(check)
 
+    #sample_indices=np.delete(sample_indices,check+1, axis=0)
+    #symbols=np.delete(symbols,check+1, axis=0)
+
+    #print(sample_indices)
+    #validate sample_indices - make sure each r_peak aligns with an annotation
+    r_peaks_matched, sample_indices_matched, symbols_matched = [], [], []
+
+    for i in range(len(r_peaks)):
+        close = np.isclose(r_peaks[i],sample_indices, atol=50)
+        close.tolist()
+        if True in close: #has to be really close to ensure that only one True is in the array
+            close_indices = np.where(close)[0][0]
+            r_peaks_matched.append(r_peaks[i])
+            sample_indices_matched.append(sample_indices[close_indices])
+            symbols_matched.append(symbols[close_indices])
+
+    # should return arrays with same values
     
-    for i in range(len(r_peaks)-1):
+    output_x = []
+    output_y = []
+    output_indices = []
+    output_test = pd.DataFrame(columns = ['index1','index2','rr_post', 'rp_dist', 'rq_dist', 'rs_dist','rt_dist','rp_ratio', 'rq_ratio', 'rs_ratio', 'rt_ratio'])
+    
+    for i in range(len(r_peaks_matched)-1):
         #find r-r distance between peaks and store in array
         #ask about r-r array being one shorter than other one and how we should deal with that
-        if i == (len(r_peaks)-1):
-            break
-        else:
-            rr_post = r_peaks[i] - r_peaks[i+1]
-    '''
+        rr_post = r_peaks_matched[i+1] - r_peaks_matched[i]
         window = []
-        if r_peaks[i] > 90 and r_peaks[i]+90 <len(data) and np.isclose(r_peaks[i], sample_indices[i], atol=20):
-                window = data[lead][r_peaks[i]-90:r_peaks[i]+90] #window
+
+        if r_peaks_matched[i] > 90 and r_peaks_matched[i]+90 <len(data):
+                window = data[lead][r_peaks_matched[i]-90:r_peaks_matched[i]+90] #window
                 ##### calculations #####
+
                 #find locations of wave peaks around r wave
                 px, py = np.argmax(window[0:40]), np.max(window[0:40])
                 qx,qy = np.argmin(window[75:85]), np.min(window[75:85])
@@ -161,17 +150,17 @@ def feature_extract(record_name):
 
                 #make array of importnat values for this window
                 window_vals = np.array([rr_post, rp_dist, rq_dist, rs_dist, rt_dist, rp_ratio, rq_ratio, rs_ratio, rt_ratio])
-                output_x = np.append(output_x, window_vals)
-            #output_y = np.append() #tbd
-        
-    '''
-    ''' prob aren't using wavelet transform
-    #use wavelet transform to extract features using wavelet module
-    db1 = pywt.Wavelet('db1')
-    coeffs = pywt.wavedec(data[lead], db1, level=3)
-    wavel = coeffs[0]
-    '''
+                output_x.append(window_vals)
 
+    output_y = symbols_matched
+    output_test['index1'] = r_peaks_matched[:len(r_peaks_matched)-1]
+    output_test['index2'] = sample_indices_matched[:len(sample_indices_matched)-1]
+    output_test[['rr_post', 'rp_dist', 'rq_dist', 'rs_dist','rt_dist','rp_ratio', 'rq_ratio', 'rs_ratio', 'rt_ratio']]  = output_x
+    output_test['notes'] = symbols_matched[:len(symbols_matched)-1]
+
+    output_test.to_csv('lol.csv', index=False)
+
+    '''
     ######################## plots #########################
     fig, axs = plt.subplots(2, 1, figsize=(10, 5))
     
@@ -180,7 +169,8 @@ def feature_extract(record_name):
     axs[0].plot(data['Time'][r_peaks], data[lead][r_peaks], 'x')
     axs[0].set_title('2nd derivative')
     axs[0].grid(True)
-    
+    '''
+
     #plot of double filtered data
     '''    axs[1].plot(data['Time'],data[lead])
     axs[1].plot(data['Time'][sample_indices], data[lead][sample_indices], 'x')
@@ -188,20 +178,20 @@ def feature_extract(record_name):
     axs[1].grid(True)
     plt.tight_layout()
     plt.show()'''
-    axs[1].plot(data['Time'],data[lead])
-    axs[1].plot(data['Time'][peak_dev1], data[lead][peak_dev1], 'x')
-    axs[1].plot(data['Time'][sample_indices], data[lead][sample_indices], 'r.')
-    axs[1].set_title('1st derivative')
-    axs[1].grid(True)
+
+    plt.figure()
+    plt.plot(data['Time'],data[lead])
+    plt.plot(data['Time'][r_peaks_matched], data[lead][r_peaks_matched], 'x')
+    plt.plot(data['Time'][sample_indices], data[lead][sample_indices], 'r.')
+    plt.title('1st derivative')
+    plt.grid(True)
+
     plt.tight_layout()
     plt.show()
+
     ########################################################
 
-
-    return output_x
-
 # Example usage
-#save_ecg_to_csv('103')
-feature_extract('103')
-
+save_ecg_to_csv('106')
+feature_extract('106')
 
